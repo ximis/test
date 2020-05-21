6e848fa3206ea018201ce53070e6f45974ace91d
pinrong <pinrong@pinrongdeMacBook-Pro.local>
2020-05-21 18:11:24 +0800

    add

--- test_allure_report/test_allure_step.py
+++ test_allure_report/test_allure_step.py
@@ -28,3 +28,21 @@ def test_parameterize_with_uneven_value_sets(param1, param2, param3):
 def test_step_only():
     simple_step(1,2)
     simple_step(2,3)
+
+@allure.step
+def test_case_label_step():
+    simple_step(2,3)
+    simple_step(3,2)
+
+@allure.step
+class MyClass():
+    def simple_step2(self,params1, params2):
+        pass
+
+tt = MyClass()
+def test_class_step_label():
+    tt.simple_step2(2,3)
+    tt.simple_step2(4,5)
+
+
+
